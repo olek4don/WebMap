6f9d8e67cfc7af9015100479cbad9aee2ec64b9e
olek4don <95882449+olek4don@users.noreply.github.com>
2022-08-06 19:07:05 +0200

    Finally version. Adding a  Color-Based Polygon features and a  layer control panel.
     Issue:
    2 layers don't play along each other. When both are on, then can't points the markers properly.

--- map1.py
+++ map1.py
@@ -18,15 +18,23 @@ def color_producer(elevation):
         return 'red'
 
 
-m = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="cartodb positron")
+m = folium.Map(location=[38.58, -99.09],
+               zoom_start=6, tiles="cartodb positron")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+" m", fill_color=color_producer(el), color='grey', fill=True, fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(
+        el)+" m", fill_color=color_producer(el), color='grey', fill=True, fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fgp = folium.FeatureGroup(name="Population")
 
-m.add_child(fg)
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+                            style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+m.add_child(fgv)
+m.add_child(fgp)
+m.add_child(folium.LayerControl())
+
 m.save("Map1.html")
 webbrowser.open("/home/olo/Documents/GitHub/WebMap/Map1.html")
